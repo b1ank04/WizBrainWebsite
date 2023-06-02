157c86756cf6fbf87e397ab731984e6572e65335
mark_shmarov <shmarov.mark@gmail.com>
2023-06-02 14:21:59 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,151 @@
+  async function sendFile() {
+    const fileInput = document.getElementById('image-input');
+    const file = fileInput.files[0];
+    if (!file) {
+      alert('Please, choose a file');
+      return;
+    }
+    const formData = new FormData();
+    formData.append('file', file);
+    try {
+      const response = await fetch('http://localhost:8080/api/v1/solve', {
+        method: 'POST',
+        body: formData
+      });
+      const result = await response.json();
+      const resultDiv = document.getElementById('result');
+      if (result.error == null) {
+        resultDiv.innerHTML = '<div><span style="font-weight:bold;">Your request:</span><br>' + result.request.replaceAll("\n", "<br>") + '</div><br><div><span style="font-weight:bold;">AI response:</span><br><span style="background-color: lightgreen;color:black;padding:2px;">' + result.response + '</span></div>';
+      } else {
+        resultDiv.innerText = result.error;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function translateText() {
+    const text = document.getElementById('input-area').value;
+    const newLanguage = document.getElementById('language-select').value;
+    const formData = new FormData();
+    formData.append('text', text);
+    formData.append('new_language', newLanguage);
+    const response = await fetch('http://localhost:8080/api/v1/translateText', {
+      method: 'POST',
+      body: formData      
+    })
+    const result = await response.text();
+    const resultArea = document.getElementById('result-area');
+    resultArea.innerText = result;
+  }
+
+  async function translateFile() {
+    const input = document.getElementById('drag-and-drop');
+    const file = input.files[0];
+    if (!file) {
+      alert('Please, choose a file');
+      return;
+    }
+    const newLanguage = document.getElementById('language-select').value;
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('new_language', newLanguage);
+    const response = await fetch('http://localhost:8080/api/v1/translateFile', {
+      method: 'POST',
+      body: formData      
+    })
+    const result = await response.text();
+    const resultArea = document.getElementById('result-area');
+    resultArea.innerText = result;
+  }
+
+  function switchHandler() {
+    const toggleSwitch = document.getElementById('toggle-switch');
+    const uploadField = document.getElementById('upload-field');
+    const inputArea = document.getElementById('input-area');
+    if (toggleSwitch.checked) {
+      inputArea.style.display = "none";
+      uploadField.style.display = "block";
+    } else {
+      uploadField.style.display = "none"
+      inputArea.style.display = "block";
+    }
+  }
+
+  async function summarizeInput() {
+    const text = document.getElementById('input-area').value;
+    const formData = new FormData();
+    formData.append('text', text);
+    const response = await fetch('http://localhost:8080/api/v1/summarize', {
+      method: 'POST',
+      body: formData      
+    })
+    const result = await response.text();
+    const resultArea = document.getElementById('result-area');
+    resultArea.innerText = result;
+  }
+
+  function translateInput() {
+    const toggleSwitch = document.getElementById('toggle-switch');
+    if (toggleSwitch.checked) {
+      translateFile();
+    } else {
+      translateText();
+    }
+  }
+
+  function checkSelect() {
+    const select = document.getElementById('language-select');
+    const button = document.getElementById('translate-button');
+    if (select.value === "Choose language") {
+      button.disabled = true;
+    } else {
+      button.disabled = false;
+    }
+  }
+
+  function checkSelect() {
+    const select = document.getElementById('language-select');
+    const button = document.getElementById('translate-button');
+    if (select.value === "Choose language") {
+      button.disabled = true;
+    } else {
+      button.disabled = false;
+    }
+  }
+
+  
+
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+  
+      var reader = new FileReader();
+  
+      reader.onload = function(e) {
+        $('.image-upload-wrap').hide();
+  
+        $('.file-upload-image').attr('src', e.target.result);
+        $('.file-upload-content').show();
+  
+        $('.image-title').html(input.files[0].name);
+      };
+  
+      reader.readAsDataURL(input.files[0]);
+  
+    } else {
+      removeUpload();
+    }
+  }
+  
+  function removeUpload() {
+    $('.file-upload-input').replaceWith($('.file-upload-input').clone());
+    $('.file-upload-content').hide();
+    $('.image-upload-wrap').show();
+  }
+  $('.image-upload-wrap').bind('dragover', function () {
+      $('.image-upload-wrap').addClass('image-dropping');
+    });
+    $('.image-upload-wrap').bind('dragleave', function () {
+      $('.image-upload-wrap').removeClass('image-dropping');
+  });
+  
